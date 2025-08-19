5f1657d4c774fc7971a75065a45dd8ea5c72ce43
Shivam Patel <shivamp12@hotmail.com>
2025-08-19 15:26:11 -0400

    setup.py complete, starting testing

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="code-review-prep",
+    version="0.1.0",
+    author="Shivam Patel",
+    author_email="shivamp12@hotmail.com",
+    description="CLI tool to prepare code reviews",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/code-review-prep",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "click",
+        "rich",
+        "GitPython",
+        "pathlib",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "black",
+            "flake8",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "review-ready=cli:review_ready",
+        ],
+    },
+)
+
